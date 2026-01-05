bcf62f7092a7cb6cb002760e2ab91f8cee7c2a1e
winnHsu <ch4374@nyu.edu>
2026-01-04 21:06:41 -0500

    Update js

--- js/js1.js
+++ js/js1.js
@@ -1,12 +1,20 @@
-"use strict";(self.webpackChunksolanum=self.webpackChunksolanum||[]).push([[954],{954:(e,t,a)=>{a.d(t,{default:()=>r});class r{static hoverEnabled(){return window.matchMedia("(hover: hover)").matches}static motionEnabled(){return window.matchMedia("(hover: hover)").matches}static getFocusableElements(e){return Array.from(e.querySelectorAll(':where(a, button, input:not([type="hidden"]), select, textarea, iframe, [tabindex], [contentEditable="true"])')).filter(e=>e.offsetParent&&!e.disabled&&!e.inert&&!e.closest("[inert]"))}}}}]);
+"use strict"; (self.webpackChunksolanum = self.webpackChunksolanum || []).push([[954], { 954: (e, t, a) => { a.d(t, { default: () => r }); class r { static hoverEnabled() { return window.matchMedia("(hover: hover)").matches } static motionEnabled() { return window.matchMedia("(hover: hover)").matches } static getFocusableElements(e) { return Array.from(e.querySelectorAll(':where(a, button, input:not([type="hidden"]), select, textarea, iframe, [tabindex], [contentEditable="true"])')).filter(e => e.offsetParent && !e.disabled && !e.inert && !e.closest("[inert]")) } } } }]);
 
 //below are self-defined functions
 $(document).ready(function () {
+
+  // 1. Close tooltips when clicking anywhere on the page
+  $(document).on('click', function () {
+    $('.points-of-interest-image-tooltip').removeClass('showCard');
+  });
+
+  // 2. Toggle tooltip when clicking a POI button
   $('.points-of-interest-image-info').on(
     'click',
     '.points-of-interest-image-item button.points-of-interest-image-toggle',
     function (e) {
       e.preventDefault();
+      e.stopPropagation(); // <-- critical
 
       var $tooltip = $(this).siblings('.points-of-interest-image-tooltip');
       var isOpen = $tooltip.hasClass('showCard');
