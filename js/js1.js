532465167e2a7bd299b1e42861dbd2d840a4678c
winnHsu <133544834+winnHsu@users.noreply.github.com>
2026-01-04 20:16:56 -0500

    Add files via upload

--- js/js1.js
+++ js/js1.js
@@ -1,1 +1,21 @@
 "use strict";(self.webpackChunksolanum=self.webpackChunksolanum||[]).push([[954],{954:(e,t,a)=>{a.d(t,{default:()=>r});class r{static hoverEnabled(){return window.matchMedia("(hover: hover)").matches}static motionEnabled(){return window.matchMedia("(hover: hover)").matches}static getFocusableElements(e){return Array.from(e.querySelectorAll(':where(a, button, input:not([type="hidden"]), select, textarea, iframe, [tabindex], [contentEditable="true"])')).filter(e=>e.offsetParent&&!e.disabled&&!e.inert&&!e.closest("[inert]"))}}}}]);
+
+//below are self-defined functions
+$(document).ready(function () {
+  $('.points-of-interest-image-info').on(
+    'click',
+    '.points-of-interest-image-item button.points-of-interest-image-toggle',
+    function (e) {
+      e.preventDefault();
+
+      var $tooltip = $(this).siblings('.points-of-interest-image-tooltip');
+      var isOpen = $tooltip.hasClass('showCard');
+
+      $('.points-of-interest-image-tooltip').removeClass('showCard');
+
+      if (!isOpen) {
+        $tooltip.addClass('showCard');
+      }
+    }
+  );
+});
